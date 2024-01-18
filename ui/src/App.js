7de74e9c628ac1a772d188228c9de1dd1e5693da
Shreya Rathi <72522489+shreya-rathi@users.noreply.github.com>
2024-01-18 14:24:54 -0500

    creates a basic react app

---
+++ ui/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          ECE 49595 0SS Senior Design Project
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
