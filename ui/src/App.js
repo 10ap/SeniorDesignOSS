94b0f6cd741642ca50f761c0944278b49f75eb51
aaradhyajajoo <aaradhya.jajoo@gmail.com>
2024-01-29 16:28:47 -0500

    Add webpack (#2)
    * Update to add Babel and Webpack support to "compile"
    
    * Update the extension to view at max width and height
    
    * Add defer to remove error about createRoot

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,15 +1,14 @@
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          ECE 49595 0SS Senior Design Project
-        </p>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>ECE 49595 0SS Senior Design Project</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
