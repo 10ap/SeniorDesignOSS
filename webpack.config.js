010be87270bfac3b5e8575f0e31f6bd692f2ed07
10ap <patilatharva01@gmail.com>
2024-02-27 09:15:06 -0500

    Move config files to allow build from root directory

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+	mode: "development",
+	entry: {
+		content: "./src/frontend/index.js",
+		background: "./src/backend/service-worker.js",
+	},
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "build"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/public/index.html",
+		}),
+		new webpack.ProvidePlugin({
+			browser: "webextension-polyfill",
+		}),
+	],
+	devtool: "cheap-module-source-map",
+};
