be922bfa74cb74d818f10c482bab91f38397f603
10ap <patilatharva01@gmail.com>
2024-02-27 09:42:40 -0500

    implement jest for unit testing

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // Automatically clear mock calls, instances and results before every test
+    clearMocks: true,
+  
+    // An array of glob patterns indicating a set of files for which coverage information should be collected
+    collectCoverage: true,
+  
+    // The directory where Jest should output its coverage files
+    coverageDirectory: "coverage",
+  
+    // An array of file extensions your modules use
+    moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+  
+    // The test environment that will be used for testing
+    testEnvironment: 'jest-environment-jsdom',
+    
+    // Setup files after the environment is loaded
+    setupFilesAfterEnv: ["./src/setupTests.js"], // Replace with the path to your setup file
+};
