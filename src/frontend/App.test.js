e6a5ff349f1ace2a8ed8c0677c0d9ed895d03976
10ap <patilatharva01@gmail.com>
2024-02-27 09:48:51 -0500

    added test case to render project title

--- src/frontend/App.test.js
+++ src/frontend/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the project title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/ECE 49595 0SS Senior Design Project/i);
   expect(linkElement).toBeInTheDocument();
 });
