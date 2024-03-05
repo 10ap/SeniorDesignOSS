1085cc521e199c4759b1d48151e19d14253a1375
Shreya Rathi <72522489+shreya-rathi@users.noreply.github.com>
2024-03-04 19:14:40 -0500

    piechart feature

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -9,7 +9,7 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<p>WellQuest</p>
+				<p>WellQuest Productivity Tracker</p>
 			</header>
 
 			<div className="tabs">
--- src/frontend/UsageSummary.js
+++ src/frontend/UsageSummary.js
@@ -1,15 +1,33 @@
-// implement the usage summary component
 import React, { Component } from "react";
+import { PieChart, Pie, Legend, Tooltip, Cell } from "recharts";
+import "./UsageSummary.css";
 
 class UsageSummary extends Component {
-	render() {
-		return (
-			<div>
-				<h2>Usage Summary</h2>
-				<p>Usage Summary</p>
-			</div>
-		);
-	}
+    render() {
+        const data2 = [
+            { name: 'Geeksforgeeks', students: 400, color: '#8E7AB5' },
+            { name: 'Technical scripter', students: 700, color: '#B784B7' },
+            { name: 'Geek-i-knack', students: 200, color: '#EEA5A6' },
+            { name: 'Geek-o-mania', students: 1000, color: '#E493B3' }
+        ];
+
+        return (
+            <div>
+                <h2>Usage Summary</h2>
+                <div className="pie-chart-container">
+                    <PieChart width={500} height={400}>
+                        <Pie data={data2} dataKey="students" nameKey="name" cx="50%" cy="50%" legendType="circle">
+                            {data2.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={entry.color} />
+                            ))}
+                        </Pie>
+                        <Legend />
+                        <Tooltip />
+                    </PieChart>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default UsageSummary;
