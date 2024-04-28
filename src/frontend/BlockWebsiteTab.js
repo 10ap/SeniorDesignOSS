5ce6c46e2690127df4a5b8ce83b276e1ae9c4977
aaradhyajajoo <38855824+aaradhyajajoo@users.noreply.github.com>
2024-04-28 18:44:08 -0400

    fix bug when netflix did not get blocked

--- src/frontend/BlockWebsiteTab.js
+++ src/frontend/BlockWebsiteTab.js
@@ -1,71 +1,81 @@
-import { getAllBlockedTabs, addBlockedTab, deleteBlockedTab, resetBlockedTabs } from "../backend/BlockWebsiteTab.js";
+import {
+	getAllBlockedTabs,
+	addBlockedTab,
+	deleteBlockedTab,
+	resetBlockedTabs,
+} from "../backend/BlockWebsiteTab.js";
 import React, { Component } from "react";
 import "./BlockWebsiteTab.css";
 
 class BlockWebsiteTab extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      blockedTabs: [],
-      newTabUrl: "",
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			blockedTabs: [],
+			newTabUrl: "",
+		};
+	}
 
-  componentDidMount() {
-    this.setState({ blockedTabs: getAllBlockedTabs() });
-  }
+	componentDidMount() {
+		this.setState({ blockedTabs: getAllBlockedTabs() });
+	}
 
-  handleInputChange = (event) => {
-    this.setState({ newTabUrl: event.target.value });
-  };
+	handleInputChange = (event) => {
+		this.setState({ newTabUrl: event.target.value });
+	};
 
-  addBlockedTab = (event) => {
-    event.preventDefault();
-    const { newTabUrl } = this.state;
-    if (newTabUrl) {
-      addBlockedTab(newTabUrl);
-      this.setState({ blockedTabs: getAllBlockedTabs(), newTabUrl: "" });
-    }
-  };
+	addBlockedTab = (event) => {
+		event.preventDefault();
+		const { newTabUrl } = this.state;
+		if (newTabUrl) {
+			addBlockedTab(newTabUrl);
+			this.setState({ blockedTabs: getAllBlockedTabs(), newTabUrl: "" });
+		}
+	};
 
-  removeBlockedTab = (index) => {
-    deleteBlockedTab(index);
-    this.setState({ blockedTabs: getAllBlockedTabs() });
-  };
+	removeBlockedTab = (index) => {
+		deleteBlockedTab(index);
+		this.setState({ blockedTabs: getAllBlockedTabs() });
+	};
 
-  resetBlockedTabs = () => {
-    resetBlockedTabs();
-    this.setState({ blockedTabs: [] });
-  };
+	resetBlockedTabs = () => {
+		resetBlockedTabs();
+		this.setState({ blockedTabs: [] });
+	};
 
-  render() {
-    const { blockedTabs, newTabUrl } = this.state;
-    return (
-      <div className="BlockWebsiteTab">
-        <form onSubmit={this.addBlockedTab}>
-          <input
-            type="text"
-            value={newTabUrl}
-            onChange={this.handleInputChange}
-            placeholder="Enter the website you want to block..."
-          />
-          <button type="submit">Add</button>
-        </form>
-        <h3>Blocked Websites</h3>
-        <ul>
-          {blockedTabs.map((tab, index) => (
-            <div key={index}>
-				<button className="removeButton" onClick={() => this.removeBlockedTab(index)}>-</button>
-              <span>{tab}</span>
-            </div>
-          ))}
-        </ul>
-        <button className="reset-btn" onClick={this.resetBlockedTabs}>
-          Reset All
-        </button>
-      </div>
-    );
-  }
+	render() {
+		const { blockedTabs, newTabUrl } = this.state;
+		return (
+			<div className="BlockWebsiteTab">
+				<form onSubmit={this.addBlockedTab}>
+					<input
+						type="text"
+						value={newTabUrl}
+						onChange={this.handleInputChange}
+						placeholder="Enter the website you want to block..."
+					/>
+					<button type="submit">Add</button>
+				</form>
+				<h3>Blocked Websites</h3>
+				<ul>
+					{blockedTabs.map((tab, index) => (
+						<div key={index}>
+							<button
+								className="removeButton"
+								onClick={() => this.removeBlockedTab(index)}
+							>
+								-
+							</button>
+							<span>{tab}</span>
+						</div>
+					))}
+				</ul>
+				<button className="reset-btn" onClick={this.resetBlockedTabs}>
+					Reset All
+				</button>
+			</div>
+		);
+	}
 }
 
 export default BlockWebsiteTab;
